428880fc464387af2072c793d267b7e13fd5eb03
Limulos <randomimus.beakuzz@gmail.com>
2023-08-03 20:04:22 +0200

    Added score and highscore logic

--- main.lua
+++ main.lua
@@ -4,11 +4,41 @@ Consts = require("consts")
 local images = {}
 local sounds = {}
 local texts = {}
-local music
+local music = nil
 
 local state = "menu"
 local lives = Consts.livesMax
 local targetPosition = Vector2(0, 0)
+
+local score = 0
+local highscore = 0
+local startHighscore = 0
+
+local function loadHighscore()
+    local file = io.open(Consts.highscoreFilePath)
+    if file == nil then
+        return
+    end
+
+    local content = file:read("*n")
+    if content ~= nil then
+        highscore = content
+        startHighscore = content
+        print(startHighscore)
+    end
+
+    file:close()
+end
+
+local function saveHighscore()
+    local file = io.open(Consts.highscoreFilePath, "w")
+    if file == nil then
+        return
+    end
+
+    file:write(highscore)
+    file:close()
+end
 
 local function loadImages()
     for _, filepath in ipairs(love.filesystem.getDirectoryItems("assets/img/")) do
@@ -25,10 +55,15 @@ local function loadSounds()
 end
 
 function love.load()
+    loadHighscore()
+
     local font = love.graphics.setNewFont(20)
     local bigFont = love.graphics.setNewFont(50)
-    texts.lost = love.graphics.newText(bigFont, "You lost!")
+
+    texts.lostTitle = love.graphics.newText(bigFont, "You lost!")
+    texts.lostScore = love.graphics.newText(font)
     texts.sub = love.graphics.newText(font, "Press [space] to start")
+    texts.score = love.graphics.newText(font, string.format("%02d/%02d", score, highscore))
 
     music = love.audio.newSource("assets/mus/alien_swamp.ogg", "stream")
 
@@ -38,16 +73,33 @@ end
 
 function love.mousepressed()
     if state == "game" then
-        state = "lost"
-        music:stop()
-        sounds.lost:play()
+        score = score + 1
+        texts.score:set(string.format("%02d/%02d", score, highscore))
     end
 end
 
 local function launchGame()
     state = "game"
+    score = 0
+    texts.score:set(string.format("%02d/%02d", score, highscore))
     sounds.start:play()
     music:play()
+end
+
+local function launchLost()
+    state = "lost"
+
+    local text = ""
+    if score > highscore then
+        highscore = score
+        text = string.format("%02d, new record!", score)
+    else
+        text = string.format("You got %02d", score)
+    end
+
+    texts.lostScore:set(text)
+    music:stop()
+    sounds.lost:play()
 end
 
 function love.keypressed(key)
@@ -61,6 +113,8 @@ function love.keypressed(key)
         if key == "escape" then
             state = "menu"
             music:stop()
+        elseif key == "space" then
+            launchLost()
         end
     elseif state == "lost" then
         if key == "space" then
@@ -91,8 +145,11 @@ function love.draw()
             local position = Vector2(80 + (i - 1) * (5 + Consts.heartSize), Consts.heartY)
             love.graphics.draw(images.heart, quad, position.x, position.y)
         end
+
+        love.graphics.draw(texts.score, Consts.screenWidth - texts.score:getWidth() - Consts.offset, Consts.gui.rect.y + (Consts.gui.height - texts.score:getHeight()) / 2)
     elseif state == "lost" then
-        love.graphics.draw(texts.lost, (Consts.screenWidth - texts.lost:getWidth()) / 2, Consts.screenHeight / 4 - texts.lost:getHeight() / 2)
+        love.graphics.draw(texts.lostTitle, (Consts.screenWidth - texts.lostTitle:getWidth()) / 2, Consts.screenHeight / 4 - texts.lostTitle:getHeight() / 2)
+        love.graphics.draw(texts.lostScore, (Consts.screenWidth - texts.lostScore:getWidth()) / 2, (Consts.screenHeight - texts.lostScore:getHeight()) / 2)
         love.graphics.draw(texts.sub, (Consts.screenWidth - texts.sub:getWidth()) / 2, Consts.screenHeight * 0.75 - texts.sub:getHeight() / 2)
     end
 
@@ -100,5 +157,7 @@ function love.draw()
 end
 
 function love.quit()
-    -- Write highscore
+    if highscore > startHighscore then
+        saveHighscore()
+    end
 end
